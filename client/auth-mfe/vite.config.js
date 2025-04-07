84b83f751e756843d47941da262c399918f498cf
amonani18 <165120116+amonani18@users.noreply.github.com>
2025-04-06 22:08:22 -0400

    update Vite configuration to set base URL for deployment

--- client/auth-mfe/vite.config.js
+++ client/auth-mfe/vite.config.js
@@ -4,7 +4,7 @@ import federation from '@originjs/vite-plugin-federation'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/',
+  base: 'https://community-aibot-1.onrender.com/',
   plugins: [
     react(),
     federation({
